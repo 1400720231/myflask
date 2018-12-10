ca16d197db2bffe49085f4abf779c1ef3b9c26da
mini_panda <18778331181@sina.cn>
2018-12-10 21:21:27 +0800

    完成注册功能

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from exts import db
 
 class User(db.Model):
+	# 表名字
 	__tablename__ = "user"
-
+	#　字段　id 整型　主键　自增长|django 中默认id为主键且自增长
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	telephone = db.Column(db.String(11), nullable=False)
 	username = db.Column(db.String(50), nullable=False)
--- zlktqa.py
+++ zlktqa.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from exts import db
+from models import User
 # 引入配置文件
 import config
 app = Flask(__name__)
 # 加载配置文件内容
 app.config.from_object(config)
+
+db.init_app(app)
 
 
 @app.route("/")
@@ -18,10 +21,28 @@ def login():
 		return render_template("login.html")
 
 
-@app.route("/register/")
+@app.route("/register/",methods=["GET", "POST"])
 def register():
 	if request.method == "GET":
 		return render_template("register.html")
+	else:
+		telephone = request.form.get("telephone")
+		username = request.form.get("username")
+		password1 = request.form.get("password1")
+		password2 = request.form.get("password2")
+
+		# 验证手机号码是否被注册了
+		user = User.query.filter(User.telephone == telephone).first()
+		if user:
+			return "手机号码已经被注册，请更换手机号码"
+		elif password2 != password1:
+			return "两次密码不一致，请重新输入"
+		else:
+			user = User(telephone=telephone, username=username, password = password1)
+			db.session.add(user)
+			db.session.commit()
+			return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run()
